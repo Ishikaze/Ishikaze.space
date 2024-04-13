312407616655cdbcdea2cfc7f62a870e673816f8
ishikaze <ishikaze.k@gmail.com>
2024-04-13 10:57:11 +0700

    loader

--- index.js
+++ index.js
@@ -1,10 +1,35 @@
 const http = new XMLHttpRequest;
 const debug = document.getElementById('debug')
 let x = 0
+let debugText = ''
+
+function onReady(callback) {
+    var intervalId = window.setInterval(function () {
+        if (document.getElementsByTagName('body')[0] !== undefined) {
+            window.clearInterval(intervalId);
+            callback.call(this);
+        }
+    }, 1000);
+}
+
+function setVisible(selector, visible) {
+    document.querySelector(selector).style.display = visible ? 'block' : 'none';
+}
+
+onReady(function () {
+    setVisible('.page', true);
+    setVisible('.loading', false);
+});
+
 
 function refresh() {
     x = x + 1
-    debug.innerHTML = '[refreshed ' + x + " times]"
+    debugText = '[refreshed ' + x + " times]"
 };
 
-setInterval(refresh, 1000)
+function updateDebug() {
+    debug.innerHTML = debugText
+};
+
+setInterval(refresh, 1000);
+setInterval(updateDebug, 1000);
